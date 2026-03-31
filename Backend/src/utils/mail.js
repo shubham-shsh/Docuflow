7ded7b523f660943e7c736c62e821c817fd76a4d
Shubham Sharma <kumarsharmashubham8@gmail.com>
2026-04-01 00:33:38 +0530

    purchased new domain for docuflow

--- Backend/src/utils/mail.js
+++ Backend/src/utils/mail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: `DocuFlow 👨‍💻 <${process.env.EMAIL_FROM || 'onboarding@resend.dev'}>`,
+      from: `DocuFlow 👨‍💻 <${process.env.EMAIL_FROM || "noreply@docuflow.co.in"}>`,
       to,
       subject,
       html,
